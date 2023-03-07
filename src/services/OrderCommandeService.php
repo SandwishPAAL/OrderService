14c9d5aa110fcd5c3627ec8cca575221d149905f
Arnaud Bourgoin <arnaud.bourgoin@outlook.com>
2023-03-07 15:06:50 +0100

    Decoupage Service/Action update de commande

--- src/services/OrderCommandeService.php
+++ src/services/OrderCommandeService.php
@@ -17,4 +17,19 @@ class OrderCommandeService
     {
         return Commande::select('id', 'mail as client_mail', 'nom as client_nom', 'created_at as order_date', 'livraison as delivery_date', 'montant as total_amount')->where('id', '=', $id)->first()->toJson(JSON_PRETTY_PRINT);
     }
+
+    public static function update(string $id, array $data)
+    {
+        try {
+            $order = Commande::find($id);
+            $order->nom = filter_var($data["client_nom"], FILTER_SANITIZE_SPECIAL_CHARS);
+            $order->mail = filter_var($data["client_mail"], FILTER_SANITIZE_EMAIL);
+            $order->created_at = filter_var($data["order_date"], FILTER_SANITIZE_SPECIAL_CHARS);
+            $order->livraison = filter_var($data["delivery_date"], FILTER_SANITIZE_SPECIAL_CHARS);
+
+            return $order->save();
+        } catch (\Throwable $ex) {
+            throw $ex;
+        }
+    }
 }
