5068b5efe8a19d84eeb9ebf9838ad15e1034b4a4
Baruff54 <43937533+Baruff54@users.noreply.github.com>
2023-03-13 10:21:14 +0100

    Add modif

--- src/actions/GetOrderByIdAction.php
+++ src/actions/GetOrderByIdAction.php
@@ -4,15 +4,26 @@ namespace lbs\order\actions;
 
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
+use Slim\Routing\RouteContext;
 use lbs\order\services\OrderCommandeService;
 
 final class GetOrderByIdAction
 {
     public function __invoke(Request $rq, Response $rs, mixed $args)
     {
+        $routeParser = RouteContext::fromRequest($rq)->getRouteParser();
+
         $order = OrderCommandeService::getById($args["id"]);
+        $data = [
+            "type" => "resource",
+            "order" => $order,
+            "links" => [
+                "items" => ["href" => $routeParser->urlFor("itemsByCommand", ["id" => $args["id"]])],
+                "self" => ["href" => $routeParser->urlFor("orderById", ["id" => $args["id"]])]
+            ]
+        ];
         $rs = $rs->withHeader('Content-type', 'application/json');
-        $rs->getBody()->write($order);
+        $rs->getBody()->write(json_encode($data));
 
         return $rs;
     }
